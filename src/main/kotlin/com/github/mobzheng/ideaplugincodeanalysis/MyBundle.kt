04ff086d7644175b5ff7ee7150be7c8e26fe31b1
GitHub Action <action@github.com>
2021-06-26 15:09:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/mobzheng/ideaplugincodeanalysis/MyBundle.kt
+++ src/main/kotlin/com/github/mobzheng/ideaplugincodeanalysis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mobzheng.ideaplugincodeanalysis
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mobzheng/ideaplugincodeanalysis/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mobzheng.ideaplugincodeanalysis.services
+
+import com.github.mobzheng.ideaplugincodeanalysis.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
